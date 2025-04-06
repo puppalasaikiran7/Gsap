8cd59d1bb92c60ff3d17225e6c79437a6eb3712a
sai kiran puppala <95897517+puppalasaikiran7@users.noreply.github.com>
2025-04-07 02:30:34 +0530

    Add files via upload

---
+++ basic_project/script.js
@@ -0,0 +1,27 @@
+let tl = gsap.timeline();
+
+tl.from("#nav h3" , {
+    y:-50,
+    duration : 0.8,
+    opacity :0,
+    delay: 0.4,
+    stagger : 0.2    
+})
+
+
+tl.from("#main h1", {
+    x:-500,
+    opacity:0,
+    duration:0.8,
+    stagger:0.5
+})
+
+
+tl.from("img" , {
+    x: 100,
+    rotate :45,
+    duration: 0.5,
+    opacity:0,
+    stagger:0.5
+})
+
